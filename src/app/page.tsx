96ab2f72abc8c9e5fc516c5f8dadf58dd78502b6
Raafi Muhammad <88325185+0raafi@users.noreply.github.com>
2024-05-30 02:50:18 +0700

    setup shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '../components/ui/button';
+
+export default function Home() {
+  return (
+    <main className="max-w-[430px] bottom-0 z-20 mx-auto grid min-h-screen w-full bg-white">
+      <div className="container relative min-h-screen overflow-hidden sm:mx-auto border">
+
+      </div>
+    </main>
+  );
+}
